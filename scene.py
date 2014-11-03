d7f43af13242283bae4a8ffe188404269cee7d75
Harry Milne <harry.milne@me.com>
2014-11-03 15:12:15 +0000

    repopulate menu command

--- scene.py
+++ scene.py
@@ -2,9 +2,6 @@
 
 from Tkinter import *
 from random import randint
-import time
-import Queue
-import threading
 
 class Scene(Frame): ##main canvas class (creating the window)
     def __init__(self, master=None, width=750, height=500): ##passing the Tk root + options
@@ -20,7 +17,7 @@ class Scene(Frame): ##main canvas class (creating the window)
         
         file_menu = Menu(menubar)
         file_menu.add_command(label="Repopulate", command=self.repopulate)
-        file_menu.add_command(label="Exit", command=self.on_exit)
+        file_menu.add_command(label="Exit", command=self.quit)
         menubar.add_cascade(label="File", menu=file_menu)
         self.pack()
 
@@ -28,28 +25,22 @@ class Scene(Frame): ##main canvas class (creating the window)
         self.populate()
         self.canvas.pack()
 
-    def on_exit(self):
-        print "bye"
-        self.quit()
-
     def clear(self):
         self.canvas.clear()
 
     def check_overlapping(self, x, y):
-        tested = []
-        for i in range(-50, 60, 50):
-            tested.append(self.canvas.find_overlapping(x+i, y+i, x+i+100, y+i+100))
-
-        return any(tested)
+        
+        return self.canvas.find_overlapping(x-50, y-50, x+150, y+150)
+    
 
     def populate(self, num=5): ##populate obstacles
         for sq in range(num): ##loop for how many obstacles wanted
-            x = randint(0, self.width-100) ##random x1 + y1
-            y = randint(0, self.height-100)
+            x = randint(50, self.width-100) ##random x1 + y1
+            y = randint(50, self.height-100)
 
-            while self.check_overlapping(x, y):
-                x = randint(0, self.width-100) ##random x1 + y1
-                y = randint(0, self.height-100)
+            while self.check_overlapping(x, y): ##check x, y for existing object
+                x = randint(50, self.width-100) ##random x1 + y1
+                y = randint(50, self.height-100)
             self.canvas.create_rectangle(x, y, x+100, y+100, ##create rectangle
                 outline="#f11", fill="#1f1", width=2)
 
@@ -58,7 +49,7 @@ class Scene(Frame): ##main canvas class (creating the window)
         self.populate()
         self.canvas.pack()
 
-    def start_robot(self):
+    def add_robot(self):
         ##
         pass
 
