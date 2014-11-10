9e092358c849104a318477453ece9ecd4c8a0a65
Dawid Szewc <dawid.szewc95@gmail.com>
2014-11-10 11:06:42 +0000

    Does not yet work with scene.py

--- trafficlights.py
+++ trafficlights.py
@@ -1,18 +1,22 @@
 from Tkinter import*
 import time
+from random import randint
+
+window = Tk()
+window.title("Traffic Light")
+canvas = Canvas (window, width = 750, height = 500, bg = "white")
 
 class TrafficLights:
+    global window
+    global canvas
+
     def __init__(self):
-        window = Tk()
         window.title("Traffic Light")
 
-        self.canvas = Canvas (window, width = 750, height = 500, bg = "white")
-        self.canvas.pack()
+        canvas.pack()
         frame = Frame(window)
         frame.pack()
         self.v1 = IntVar()
-
-
 
         rbRed = Radiobutton(frame, text = "Red", bg = "red",
                 variable = self.v1, value = 1,
@@ -32,13 +36,13 @@ class TrafficLights:
             elf.lbl["fg"] = "green"
 
     
-    id = self.canvas.create_rectangle(200, 67, 265, 60)
+    id = canvas.create_rectangle(200, 67, 265, 60)
     def displayRectangle(self):
-        self.canvas.create_rectangle(200, 67, 265, 60, tags = "rect")
+        rectangle1 = canvas.create_rectangle(200, 67, 265, 60, tags = "rect")
     def displayOval(self):
-        self.canvas.create_oval(10, 10, 10, 10, fill='red')
+        canvas.create_oval(10, 10, 10, 10, fill='red')
     def displayOval(self):
-        self.canvas.create_oval(30, 30, 30, 30, fill='green')
+        canvas.create_oval(30, 30, 30, 30, fill='green')
 
 TrafficLights()
 
